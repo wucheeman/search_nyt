228acac9557fd44bd69ad6cdbabf0fba61919a39
Mark Hainline <wucheeman@gmail.com>
2018-07-08 20:44:12 -0400

    Added Saved route to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Discover from "./pages/Discover";
+// import About from "./pages/About";
+// import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Saved from "./components/Saved";
+// import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        {/* <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/discover" component={Discover} /> */}
+        <Route exact path="/search" component={Saved} />
+      </Wrapper>
+      {/* <Footer /> */}
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Saved/Saved.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Saved.css";
+
+const Saved = props => (
+  <p>Reached the Saved page</p>
+);
+
+export default Saved;
