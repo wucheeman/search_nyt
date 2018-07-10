0b2de8138f3ac2cc1bea6258b0d71a9c2b81c98c
Mark Hainline <wucheeman@gmail.com>
2018-07-10 16:36:39 -0400

    jumbotron components

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Jumbotron.css";
+
+
+const Jumbotron = props => (
+
+  <div className="jumbotron">
+    <h1 className="text-center">
+      <strong>
+        <i className="fa fa-newspaper-o"></i> New York Times Search</strong>
+    </h1>
+  </div>
+
+);
+
+export default Jumbotron;
