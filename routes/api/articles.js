d940ead7f007f0938c51359a2e4d2fa0945cedc5
Mark Hainline <wucheeman@gmail.com>
2018-07-11 17:06:22 -0400

    added get route to api/articles on server side

---
+++ routes/api/articles.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const articlesController = require("../../controllers/articlesController");
+
+// Matches with "/api/articles"
+router.route("/")
+  .get(articlesController.findAll)
+  // TODO: update to work with articles
+  // .post(booksController.create);
+
+
+// TODO: update to work with articles
+// Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   .get(booksController.findById)
+//   .put(booksController.update)
+//   .delete(booksController.remove);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,19 +7,18 @@ const PORT = process.env.PORT || 3001;
 
 console.log('server.js lives!');
 
-// TODO: uncomment when ready to test
-// // Define middleware here
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-// // Add routes, both API and view
-// app.use(routes);
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-// // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytarticles");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytarticles");
 
 // Start the API server
 app.listen(PORT, function() {
