168ff3d5c13f1570892d3ef8fdfc4a5f40d16556
Mark Hainline <wucheeman@gmail.com>
2018-07-13 17:24:48 -0400

    clean up of dead code

--- client/src/components/NoMatch/NoMatch.js
+++ client/src/components/NoMatch/NoMatch.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Jumbotron from "../Jumbotron";
 
 const NoMatch = () => (
   <div className='container'>
   <div className="row">
     <div className="col-sm-12 text-center">
-        {/* <Jumbotron> */}
           <h1>404</h1>
           <h2>This is not the web page you were looking for</h2>
           <h1>
@@ -13,7 +11,6 @@ const NoMatch = () => (
               🙄
             </span>
           </h1>
-        {/* </Jumbotron> */}
       </div>
     </div>
   </div>
