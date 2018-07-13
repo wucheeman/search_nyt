0c3e8deb149f876302b7c6bd294c832540171f7b
Mark Hainline <wucheeman@gmail.com>
2018-07-13 17:15:38 -0400

    clean up of dead code in Saved component

--- client/src/components/Saved/Saved.js
+++ client/src/components/Saved/Saved.js
@@ -7,12 +7,6 @@ import "./Saved.css";
 
 class Saved extends Component {
   state = {
-    // TODO: delete
-    // searchTerm: '',
-    // startYear: '',
-    // endYear: '',
-    // results: [],
-    // error: ''
     articles: [], // will be array of saved articles
     id: "",
     headline: "",
@@ -43,24 +37,12 @@ class Saved extends Component {
   handleClick = event => {
     let value = event.target.value;
     const name = event.target.name;
-    // console.log(name, value);
     if (name==='viewArticle') {
       window.open(value, '_blank');
     }
     else if (name==='deleteArticle') {
-      //console.log(`Need to delete the article with id ${value}`);
       this.deleteArticle(value);
     }
-    //  TODO: delete
-    // else if (name==='clearAll') {
-    //   this.setState({searchTerm: ''});
-    //   this.setState({startYear: ''});
-    //   this.setState({endYear: ''});
-    //   this.setState({results: []});
-    //   // not resetting
-    //   // this.state.error = '';
-    // } 
-
   }
 
   render() {
@@ -87,15 +69,11 @@ class Saved extends Component {
               {/* TODO: improve ID */}
               {/* individual articles go in cards here */}
               <div className="card-body" id="well-section">
-                {/* {console.log(this.state.articles)} */}
                 {this.state.articles.map(article => (
                   // start of article card
                   <div className='row border-top border-bottom'>
 
                     <div className='col-sm-9 pt-2'>
-                      {/* start of content card */}
-                      {/* TODO: delete key value? doesn't seem to work */}
-                      {/* <div className='card' key={article.web_url}> */}
                         <h4>
                           <strong>
                             {article.headline}
@@ -107,14 +85,10 @@ class Saved extends Component {
                         <h6>
                           {article.pub_date}
                         </h6>
-                       {/* end of content card */}
-                      {/* </div> */}
                     {/* End of col-sm-9 */}
                     </div>
 
                     <div className='col-sm-3 d-flex align-items-center'>
-                      {/* start of button card */}
-                      {/* <div className='card' key={article.web_url}> */}
                         <div className='btn-group vertical'>
                           <form>
                             <button
@@ -139,8 +113,6 @@ class Saved extends Component {
                             </button>
                           </form>
                         </div>
-                      {/* end of button card */}
-                      {/* </div> */}
                     {/* end of col-sm-4 */}
                     </div>
 
